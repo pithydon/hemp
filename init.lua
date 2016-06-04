ccf6817be2f4743a8d8067ec8f59c5e6db2b6d3c
Napiophelios <Napiophelios@gmail.com>
2016-06-04 15:24:33 -0400

    add fabric to wool group add alt recipe for default paper

--- init.lua
+++ init.lua
@@ -282,6 +282,7 @@ minetest.register_craftitem("hemp:hemp_fiber", {
 minetest.register_craftitem("hemp:hemp_fabric", {
 	description = "Hemp Fabric",
 	inventory_image = "hemp_hemp_fabric.png",
+	groups = {wool = 1},
 })
 
 -- craft recipes
@@ -303,6 +304,13 @@ minetest.register_craft({
 	recipe = {
 		{"hemp:hemp_fiber", "hemp:hemp_fiber"},
 		{"hemp:hemp_fiber", "hemp:hemp_fiber"},
+	}
+})
+
+minetest.register_craft({
+	output = "default:paper 4",
+	recipe = {
+		{"hemp:hemp_fiber"},
 	}
 })
 
